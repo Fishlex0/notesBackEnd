b9a4c3629db7c40cb64541566db23f4136350a50
alex <alexiancu397@gmail.com>
2021-10-28 17:58:26 +0300

    Add READ_ME

---
+++ READE_ME.md
@@ -0,0 +1,2 @@
+# HOW TO RUN
+To run the project execute *npm run start* inside the root project folder
--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -8,7 +8,7 @@ exports.categories = async (req, res) => {
     res.status(200).json(categories);
   } catch (error) {
     console.log('Error: ', error);
-    
+
     res.status(500).json({
       error: DEFAULT_ERROR_MESSAGE,
     });
